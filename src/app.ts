537444618aba869322126c9ee1675d8c9f88a603
colbr <colbr243@gmail.com>
2021-10-28 02:56:48 +1300

    hotfix: default redirect Added a default redirect for browser users who either get an error or land on the root URL

--- src/app.ts
+++ src/app.ts
@@ -28,8 +28,14 @@ app.use(express.json());
 app.use('/:domain', share);
 
 app.use((err: Error, req: express.Request, res: express.Response) => {
-  console.error(err);
-	res.send(err);
+	console.error(err);
+	const ua = req.headers['user-agent'];
+
+	if (/^(facebookexternalhit)|(Twitterbot)|(Pinterest)/gi.test(ua!)) {
+		res.status(500).send();
+	} else {
+		res.redirect('https://market.wilderworld.com/');
+	}
 });
 
 export = app;
